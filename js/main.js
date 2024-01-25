e5d49079c62f1912132b0e72bc8ab4a84afd6152
Shoshi <sho3066@gmail.com>
2024-01-25 10:15:57 +0200

    Make the function better

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 function onBallClick(elBall){
     var num = parseInt(elBall.innerText)
     num+=50
+    if(num===450) num=100
     elBall.innerText=num
     elBall.style.width=`${num}px`
     elBall.style.height=`${num}px`
