c912c21af2f38b4ef3f2f0b9ef132ba8bfe8f8bf
Shoshi <sho3066@gmail.com>
2024-01-25 10:08:54 +0200

    Increase ball size

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 
-function onBallClick(){
-    console.log('hi')
+function onBallClick(elBall){
+    var num = parseInt(elBall.innerText)
+    num+=50
+    elBall.innerText=num
+    elBall.style.width=`${num}px`
+    elBall.style.height=`${num}px`
 }
