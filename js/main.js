ff763525363d52a8eea58fffdb77ed7b980fc779
Shoshi <sho3066@gmail.com>
2024-01-25 10:33:52 +0200

    Add get randon int

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
 function onBallClick(elBall){
     var num = parseInt(elBall.innerText)
-    num+=50
-    if(num===450) num=100
+    num+=getRandomInt(20, 60)
+    if(num>400) num=100
     elBall.innerText=num
     elBall.style.width=`${num}px`
     elBall.style.height=`${num}px`
